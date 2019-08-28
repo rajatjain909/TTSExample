c4482ab7023c9589038004ceb5a9490d45047f53
chaitendra <chaitendrapratap@gmail.com>
2019-08-28 15:29:51 +0530

    TTS for html string TTs highlight when its start speak

--- app/src/main/java/com/example/rajat/rxjavaexmpl/MainActivity.java
+++ app/src/main/java/com/example/rajat/rxjavaexmpl/MainActivity.java
@@ -6,7 +6,9 @@ import android.graphics.Typeface;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.TextUtils;
@@ -19,13 +21,11 @@ import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.Locale;
-import java.util.regex.Pattern;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
@@ -33,13 +33,19 @@ import io.reactivex.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener, TextToSpeech.OnUtteranceCompletedListener {
 
     TextView mTextView;
-    String mString = "an enlightening experience. the process of receiving or giving systematic instruction, especially at a school or university. the theory and practice of teaching. Sachin ji Bhai. Shelendra Sir. Bhai bhai";
+    String mString = "<ul>\n" +
+            "\t&#9679;\n" +
+            "We see many different plants in our surroundings.<br/><br/>\t&#9679;\n" +
+            "Plants are<strong> </strong>broadly classified into trees, shrubs, herbs, climbers and creepers.<br/><br/>\t&#9679;\n" +
+            "Plants have two parts - root and shoot.<br/><br/>\t&#9679;\n" +
+            "The root usually grows towards the soil and shoot grows towards the sunlight.<br/><br/></ul>\n";
+    // String mString = "an enlightening experience. the process of receiving or giving systematic instruction, especially at a school or university. the theory and practice of teaching. Sachin ji Bhai. Shelendra Sir. Bhai bhai";
     /*String mString = "This technology is rapidly changing the world around us, many people worry that technology will replace human intelligence. Some educators worry that there will be no students to teach anymore in the near future as technology might take over a lot of tasks and abilities that we have been teaching our students for decades.";*/
     String[] mWordStrings, mSentenceString;
     int mSpan = 0;
-    private TextToSpeech mTextToSpeech;
     ImageButton mButton;
     boolean mStopTTS = false;
+    private TextToSpeech mTextToSpeech;
 
     @SuppressLint("CheckResult")
     @Override
@@ -50,8 +56,8 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         mTextView = findViewById(R.id.nameView);
         mButton = findViewById(R.id.buttonClick);
         mString = mString.replaceAll("  ", " ").trim();
-        mTextView.setText(mString);
-        mWordStrings = mString.split(" ");
+        mTextView.setText(Html.fromHtml(mString));
+        mWordStrings = android.text.Html.fromHtml(mString).toString().split(" ");
         /*Pattern p = Pattern.compile("(?<=\\w[\\w\\)\\]][\\.\\?\\!]\\s)");
         mSentenceString = p.split(mString);*/
         mTextToSpeech = new TextToSpeech(this, this);
@@ -65,7 +71,8 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
                     speakLoud(mString);
                     mButton.setTag("stop");
                     mButton.setImageResource(R.drawable.action_stop);
-                    highlight(mString);
+
+
                     mStopTTS = false;
                 } else if (mButton.getTag().equals("stop")) {
                     stopSpeech();
@@ -78,6 +85,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         });
     }
 
+    @SuppressLint("CheckResult")
     private void highlight(final String string) {
 
         Observable.create(new ObservableOnSubscribe<SpannableStringBuilder>() {
@@ -86,11 +94,12 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
                 if (!TextUtils.isEmpty(string)) {
                     for (int j = 0; j < mWordStrings.length; j++) {
                         String speakWord = mWordStrings[j];
-                        SpannableStringBuilder stringBuilder = new SpannableStringBuilder(mString);
+
+                        SpannableStringBuilder stringBuilder = new SpannableStringBuilder(android.text.Html.fromHtml(mString).toString());
                         stringBuilder.setSpan(new ForegroundColorSpan(Color.BLUE), mSpan, mSpan + speakWord.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
                         mSpan = mSpan + speakWord.length() + 1;
                         emitter.onNext(stringBuilder);
-
                         synchronized (emitter) {
                             /*For English*/
                             if (speakWord.contains("etc.") || speakWord.contains(".") || speakWord.contains("?") || speakWord.contains("!!!") || speakWord.contains("।") || speakWord.contains("|")) {
@@ -120,7 +129,9 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
                 .subscribe(new Consumer<SpannableStringBuilder>() {
                     @Override
                     public void accept(SpannableStringBuilder s) throws Exception {
+                        // Toast.makeText(MainActivity.this,"s"+s, Toast.LENGTH_SHORT).show();
                         mTextView.setText(s);
+
                     }
                 }, new Consumer<Throwable>() {
                     @Override
@@ -131,14 +142,16 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
                     @Override
                     public void run() throws Exception {
                         //Toast.makeText(MainActivity.this,"Success", Toast.LENGTH_SHORT).show();
-                        SpannableStringBuilder stringBuilder = new SpannableStringBuilder(mString);
+                        SpannableStringBuilder stringBuilder = new SpannableStringBuilder((mString));
                         stringBuilder.setSpan(new StyleSpan(Typeface.NORMAL), 0, mString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                        mTextView.setText(stringBuilder);
+                        mTextView.setText(Html.fromHtml(stringBuilder.toString()));
                         mSpan = 0;
                     }
                 });
     }
 
+
+    @SuppressLint("CheckResult")
     private void highlightSentence(final String string) {
 
         Observable.create(new ObservableOnSubscribe<SpannableStringBuilder>() {
@@ -211,15 +224,19 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
                 result = mTextToSpeech.setLanguage(Locale.US);
             }
             //  int result = mTextToSpeech.setLanguage(Locale.ENGLISH);
+
             mTextToSpeech.setPitch(1.0f);
             mTextToSpeech.setSpeechRate(0.9f);
             mTextToSpeech.setOnUtteranceCompletedListener(this);
             if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                 Log.e("After Success", "onInit: lang no available");
             }
+            mButton.setVisibility(View.VISIBLE);
             mButton.setEnabled(true);
         } else {
             Log.e("Error", "onInit: Some kind of error");
+            mButton.setVisibility(View.GONE);
+            mButton.setEnabled(false);
         }
     }
 
@@ -228,7 +245,24 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         hash.put(TextToSpeech.Engine.KEY_PARAM_STREAM, String.valueOf(AudioManager.STREAM_NOTIFICATION));
         hash.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "FINISH");
         if (!TextUtils.isEmpty(s)) {
-            mTextToSpeech.speak(s, TextToSpeech.QUEUE_ADD, hash);
+
+            mTextToSpeech.speak(android.text.Html.fromHtml(s).toString(), TextToSpeech.QUEUE_ADD, hash);
+            mTextToSpeech.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+                @Override
+                public void onStart(String s) {
+                    highlight(mString);
+                }
+
+                @Override
+                public void onDone(String s) {
+
+                }
+
+                @Override
+                public void onError(String s) {
+
+                }
+            });
 
         } else {
             mTextToSpeech.speak("No Data", TextToSpeech.QUEUE_ADD, hash);
@@ -237,6 +271,16 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 
     @Override
     protected void onDestroy() {
+        if (mTextToSpeech != null) {
+            mTextToSpeech.stop();
+            mTextToSpeech.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
         if (mTextToSpeech != null) {
             mTextToSpeech.stop();
             mTextToSpeech.shutdown();
